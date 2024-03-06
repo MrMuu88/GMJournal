fb586cb3fd9a09c8e48cf76ff848417523660d6f
Tamás József Simon <simont@infomix.hu>
2024-03-06 23:29:13 +0100

    Installed vvarious packages and resolved deps

--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
+import { FAB } from "@rneui/themed";
 
 
 export const Page1 = () => {
     return (
-        <View>
-            <Text>This is the first Page</Text>
+        <View style={{ flexGrow: 1 }}>
+            <Text style={{ color: 'black' }}>this is the first page</Text>
+            <FAB style={{ margin: 25 }} visible={true} placement="right" icon={{ name: '3d-rotation', color: 'white' }} color="green" size="medium" />
         </View>
     );
 };
@@ -16,4 +18,5 @@ export const Page2 = () => {
         </View>
     );
 };
+
 
