360fca5cd1b9069affea792ef377b9f0ac388cac
Tamás József Simon <simont@infomix.hu>
2024-03-07 15:39:22 +0100

    moved the custom drawer code out from app.js into it's own file

--- App.js
+++ App.js
@@ -3,29 +3,10 @@ import 'react-native-gesture-handler'; //acording to documentation this is neede
 
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { DrawerContentScrollView, DrawerItem, DrawerItemList, createDrawerNavigator } from "@react-navigation/drawer";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { JournalPage } from './src/Pages/JournalPage.js';
 import { InDevelopment } from './src/Pages/InDevelopment.js';
-import { View, Text } from 'react-native';
-
-
-const CustomDrawer = (props) => {
-
-  return (
-    <View style={{ flex: 1 }}>
-      <View>
-        <Text style={{ fontWeight: 'bold', fontSize: 28, color: 'white', backgroundColor: 'red', padding: 20 }}>GM Journal</Text>
-      </View>
-      <DrawerContentScrollView {...props}>
-        <DrawerItemList  {...props} />
-      </DrawerContentScrollView>
-      <View style={{ borderTopWidth: 1 }} >
-        <DrawerItem {...props} label="About" component={InDevelopment} />
-        <DrawerItem {...props} label="Settings" component={InDevelopment} />
-      </View>
-    </View>
-  );
-};
+import { CustomDrawer } from './src/Components/CustomDrawer.js';
 
 const App = () => {
 
@@ -33,7 +14,7 @@ const App = () => {
 
   return (
     <NavigationContainer>
-      <Drawer.Navigator drawerContent={(props) => <CustomDrawer {...props} />}>
+      <Drawer.Navigator drawerContent={(props) => CustomDrawer({ ...props })}>
         <Drawer.Screen name="Journals" component={JournalPage} />
         <Drawer.Screen name="Monsters" component={InDevelopment} />
         <Drawer.Screen name="Spells" component={InDevelopment} />
--- src/Components/CustomDrawer.js
+++ src/Components/CustomDrawer.js
@@ -1,11 +1,20 @@
-import { DrawerItem } from '@react-navigation/drawer';
-import { DrawerContentScrollView, DrawerItemList } from 'react-native';
+import { DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import { View, Text } from 'react-native';
 
 export const CustomDrawer = (props) => {
 
     return (
-        <DrawerContentScrollView {...props}>
-            <DrawerItemList {...props} />
-        </DrawerContentScrollView>
+        <View style={{ flex: 1 }}>
+            <View>
+                <Text style={{ fontWeight: 'bold', fontSize: 28, color: 'white', backgroundColor: 'red', padding: 20 }}>GM Journal</Text>
+            </View>
+            <DrawerContentScrollView {...props}>
+                <DrawerItemList  {...props} />
+            </DrawerContentScrollView>
+            <View style={{ borderTopWidth: 1 }} >
+                <DrawerItem {...props} label="About" />
+                <DrawerItem {...props} label="Settings" />
+            </View>
+        </View>
     );
 };
