b7f73032ab083c951bb9a556d4fcc028d06ad971
Tamás József Simon <simont@infomix.hu>
2024-03-07 12:39:40 +0100

    addButtons Component is complete

--- src/Components/AddButtons.js
+++ src/Components/AddButtons.js
@@ -1,20 +1,28 @@
-import { View } from "react-native";
+import { View, Alert, StyleSheet } from "react-native";
 import { useState } from "react";
 import { FAB } from "@rneui/themed";
 
 export const AddButtons = () => {
-    const [fabsVisible, setFabsVisible] = useState(false);
+    const [fabsVisible, setFabsVisible] = useState(true);
 
+    const createLink = () => { Alert.alert("creating a new link"); };
+    const createNote = () => { Alert.alert("creating a new note"); };
+    const createTable = () => { Alert.alert("creating a new table"); };
+    const createImage = () => { Alert.alert("creating a new image"); };
+    const createSubPage = () => { Alert.alert("creating a new subpage"); };
     return (
-        <View>
-            <FAB style={{ margin: 25 }} onPress={() => setFabsVisible(!fabsVisible)} visible={true} placement="right" icon={{ name: 'add', color: 'white' }} color="green" size="medium" />
-            <FAB style={{ marginRight: 32, marginBottom: 90 }} visible={fabsVisible} placement="right" icon={{ name: 'link', color: 'white' }} color="purple" size="small" title="link" />
-            <FAB style={{ marginRight: 32, marginBottom: 140 }} visible={fabsVisible} placement="right" icon={{ name: 'note', color: 'white' }} color="darkgreen" size="small" title="Note" />
-            <FAB style={{ marginRight: 32, marginBottom: 190 }} visible={fabsVisible} placement="right" icon={{ name: 'view-list', color: 'white' }} color="blue" size="small" title="table" />
-            <FAB style={{ marginRight: 32, marginBottom: 240 }} visible={fabsVisible} placement="right" icon={{ name: 'image', color: 'white' }} color="orange" size="small" title="image" />
-            <FAB style={{ marginRight: 32, marginBottom: 290 }} visible={fabsVisible} placement="right" icon={{ name: 'map', color: 'white' }} color="crimson" size="small" title="map" />
-            <FAB style={{ marginRight: 32, marginBottom: 340 }} visible={fabsVisible} placement="right" icon={{ name: 'layers', color: 'white' }} color="crimson" size="small" title="subpage" />
+        <View style={{ position: 'absolute', bottom: 50, right: 30, alignItems: "flex-end" }}>
+            <FAB style={styles.fab} onPress={() => { setFabsVisible(false); createSubPage() }} visible={fabsVisible} icon={{ name: 'layers', color: 'white' }} color="crimson" size="small" title="subpage" />
+            <FAB style={styles.fab} onPress={() => { setFabsVisible(false); createNote() }} visible={fabsVisible} icon={{ name: 'note', color: 'white' }} color="darkgreen" size="small" title="note" />
+            <FAB style={styles.fab} onPress={() => { setFabsVisible(false); createTable() }} visible={fabsVisible} icon={{ name: 'view-list', color: 'white' }} color="blue" size="small" title="table" />
+            <FAB style={styles.fab} onPress={() => { setFabsVisible(false); createImage() }} visible={fabsVisible} icon={{ name: 'image', color: 'white' }} color="orange" size="small" title="image" />
+            <FAB style={styles.fab} onPress={() => { setFabsVisible(false); createLink() }} visible={fabsVisible} icon={{ name: 'link', color: 'white' }} color="purple" size="small" title="link" />
+            <FAB style={styles.fab} onPress={() => setFabsVisible(!fabsVisible)} visible={true} icon={{ name: 'add', color: 'white' }} color="green" size="medium" />
         </View>
     );
-}
 
+
+}
+const styles = StyleSheet.create({
+    fab: { padding: 5 }
+});
--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -1,31 +1,17 @@
 
-import { View, Text, Alert } from "react-native";
+import { View, Text } from "react-native";
 import { AddButtons } from "../Components/AddButtons";
-import { useState } from "react";
-import { FAB } from "@rneui/themed";
 
 
 export const Page1 = () => {
-    const [fabsVisible, setFabsVisible] = useState(false);
-
-    const createLink = () => { Alert.alert("creating a new link"); };
-    const createNote = () => { Alert.alert("creating a new note"); };
-    const createTable = () => { Alert.alert("creating a new table"); };
-    const createImage = () => { Alert.alert("creating a new image"); };
-    const createSubPage = () => { Alert.alert("creating a new subpage"); };
-
     return (
         <View style={{ flexGrow: 1 }}>
             <Text style={{ color: 'black' }}>This is the first page</Text>
-            <FAB onPress={() => setFabsVisible(!fabsVisible)} style={{ margin: 25 }} visible={true} placement="right" icon={{ name: 'add', color: 'white' }} color="green" size="medium" />
-            <FAB onPress={() => { setFabsVisible(false); createLink() }} style={{ marginRight: 25, marginBottom: 90 }} visible={fabsVisible} placement="right" icon={{ name: 'link', color: 'white' }} color="purple" size="small" title="link" />
-            <FAB onPress={() => { setFabsVisible(false); createNote() }} style={{ marginRight: 25, marginBottom: 140 }} visible={fabsVisible} placement="right" icon={{ name: 'note', color: 'white' }} color="darkgreen" size="small" title="note" />
-            <FAB onPress={() => { setFabsVisible(false); createTable() }} style={{ marginRight: 25, marginBottom: 190 }} visible={fabsVisible} placement="right" icon={{ name: 'view-list', color: 'white' }} color="blue" size="small" title="table" />
-            <FAB onPress={() => { setFabsVisible(false); createImage() }} style={{ marginRight: 25, marginBottom: 240 }} visible={fabsVisible} placement="right" icon={{ name: 'image', color: 'white' }} color="orange" size="small" title="image" />
-            <FAB onPress={() => { setFabsVisible(false); createSubPage() }} style={{ marginRight: 25, marginBottom: 290 }} visible={fabsVisible} placement="right" icon={{ name: 'layers', color: 'white' }} color="crimson" size="small" title="subpage" />
+            <AddButtons />
         </View>
     );
 };
+
 
 export const Page2 = () => {
     return (
