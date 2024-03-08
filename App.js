654e6d85f027c195b64f4ba79efd36e62863a51d
Tamás József Simon <simont@infomix.hu>
2024-03-08 13:09:06 +0100

    created a simple menu

--- App.js
+++ App.js
@@ -7,20 +7,23 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { JournalPage } from './src/Pages/JournalPage.js';
 import { InDevelopment } from './src/Pages/InDevelopment.js';
 import { CustomDrawer } from './src/Components/CustomDrawer.js';
+import { MenuProvider } from 'react-native-popup-menu';
 
 const App = () => {
 
   const Drawer = createDrawerNavigator();
 
   return (
-    <NavigationContainer>
-      <Drawer.Navigator drawerContent={(props) => CustomDrawer({ ...props })}>
-        <Drawer.Screen name="Journals" component={JournalPage} />
-        <Drawer.Screen name="Monsters" component={InDevelopment} />
-        <Drawer.Screen name="Spells" component={InDevelopment} />
-        <Drawer.Screen name="Items" component={InDevelopment} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <MenuProvider>
+      <NavigationContainer>
+        <Drawer.Navigator drawerContent={(props) => CustomDrawer({ ...props })}>
+          <Drawer.Screen name="Journals" component={JournalPage} />
+          <Drawer.Screen name="Monsters" component={InDevelopment} />
+          <Drawer.Screen name="Spells" component={InDevelopment} />
+          <Drawer.Screen name="Items" component={InDevelopment} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </MenuProvider>
   );
 };
 
---
+++ src/Components/SimpleMenu.js
@@ -0,0 +1,47 @@
+//https://www.npmjs.com/package/react-native-popup-menu
+
+import { Alert, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Menu, MenuOptions, MenuOption, MenuTrigger } from "react-native-popup-menu";
+import { Icon } from "@rneui/themed";
+
+export const SimpleMenu = () => {
+    return (
+        <Menu>
+            <MenuTrigger customStyles={{
+            }}>
+                <Icon type="material-community" name="dots-vertical" color="#000000" />
+            </MenuTrigger>
+            <MenuOptions>
+                <MenuOption onSelect={() => Alert.alert("save")} style={style.MenuItem}>
+                    <Icon name="edit" color="black" />
+                    <Text style={style.menuText}>Edit</Text>
+                </MenuOption>
+                <MenuOption onSelect={() => Alert.alert("save")} style={style.MenuItem}>
+                    <Icon name="content-copy" type="material-community" color="black" />
+                    <Text style={style.menuText}>Copy</Text>
+                </MenuOption>
+                <MenuOption onSelect={() => Alert.alert("save")} style={style.MenuItem}>
+                    <Icon name="cut" type="font-awesome-5" color="black" />
+                    <Text style={style.menuText}>Cut</Text>
+                </MenuOption>
+                <MenuOption onSelect={() => Alert.alert("save")} style={style.MenuItem}>
+                    <Icon name="delete" color="black" type="material-community" />
+                    <Text style={style.menuText}>Delete</Text>
+                </MenuOption>
+
+            </MenuOptions>
+        </Menu>
+    );
+};
+
+const style = StyleSheet.create({
+    MenuItem: {
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    menuText: {
+        color: 'black'
+    }
+});
