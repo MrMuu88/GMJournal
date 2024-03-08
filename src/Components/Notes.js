a1bd0c31ade291296017ab3bbc6e6e18c61da149
Tamás József Simon <simont@infomix.hu>
2024-03-08 13:09:37 +0100

    wokring on Notes component of a JournalEntry

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,10 +1,22 @@
 import { View, Text } from "react-native";
+import { Card } from '@rneui/themed';
+import { SimpleMenu } from "./SimpleMenu";
 
 export const Notes = () => {
 
     return (
-        <View>
-            <Text>This is the first note</Text>
-        </View>
+        <Card>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                {/*ide kell egy icon a név elé*/}
+                <Text style={{ color: 'black', fontWeight: 'bold', fontSize: 24 }}>This is the Note Title</Text>
+                <SimpleMenu />
+                {/* id még kell egy ... gyors menu gomb: move, copy, delete, configure? */}
+                {/* ha ügyesen meg tudom oldani az edit-et, akkor lehet nem is kell a configure*/}
+            </View>
+            <Card.Divider />
+            <View>
+                <Text style={{ color: 'black', fontSize: 16 }}>This is the Content of the note</Text>
+            </View>
+        </Card>
     );
 };
